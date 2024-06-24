4eaf326dbde397e81b67e888648edf2665d1d52c
Connor Watson <connor.watson@gmail.com>
2024-06-24 10:16:24 -0400

    Add files via upload

---
+++ three_led_blink.ino
@@ -0,0 +1,38 @@
+#define LED_1 8
+#define LED_3 9
+#define LED_2 10
+#define button 7
+
+int buttonState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(button);
+  if (buttonState == HIGH) {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    delay(1000);
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, LOW);
+    delay(1000);
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, HIGH);
+    delay(1000);
+  } else {
+    // turn LED off:
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+  }
+}
